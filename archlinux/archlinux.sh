5540121a2eb14a508f983b6d21cc7a60defbfb0f
ecabott <ayush.bhusal0@gmail.com>
2024-11-17 08:54:14 +0545

    updated

--- archlinux/archlinux.sh
+++ archlinux/archlinux.sh
@@ -11,6 +11,7 @@ do
             echo "Installing $option."
             sudo pacman -S --needed --noconfirm - < gnome/packages.txt
             xargs flatpak install -y < gnome/flatpaks.txt
+            xargs sudo systemctl enable < gnome/services.txt
             break;;
         "hyprland")
             echo "Installation of $option not yet implemented"
