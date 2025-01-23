6494099cd0e6f5ac54fafedc215bf12f55d24078
ecabott <ayush.bhusal0@gmail.com>
2025-01-24 00:15:39 +0545

    secureboot setup

---
+++ archlinux/secureboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo pacman -S --needed --noconfirm sbctl
+
+sbctl status
+sudo sbctl create-keys
+sudo sbctl enroll-keys -m
+sbctl status
+
+sudo sbctl verify
+sudo sbctl sign -s /boot/vmlinuz-linux
+sudo sbctl sign -s /boot/EFI/Linux/arch-linux.efi
+sudo sbctl sign -s /boot/EFI/Linux/arch-linux-fallback.efi
+sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+sudo bootctl install
+sudo sbctl verify
