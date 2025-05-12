f8f8abed13aede354498fbefe2e0ecf2dd6e6683
ecabott <ayush.bhusal0@gmail.com>
2025-05-12 23:24:52 +0545

    added --needed

--- archlinux.sh
+++ archlinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm gdm alacritty nano zsh git timeshift switcheroo-control gnome-control-center fwupd nvidia-open gnome-backgrounds flatpak gnome-software noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation gnome-shell-extension-appindicator
+sudo pacman -S --needed --noconfirm gdm alacritty nano zsh git timeshift switcheroo-control gnome-control-center fwupd nvidia-open gnome-backgrounds flatpak gnome-software noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation gnome-shell-extension-appindicator
 flatpak update -y
 flatpak install -y org.mozilla.firefox com.mattjakeman.ExtensionManager
 sudo systemctl enable gdm bluetooth switcheroo-control
