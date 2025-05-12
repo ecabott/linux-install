19e658bf86b96da934078fa1580f6e36b8786d09
ecabott <ayush.bhusal0@gmail.com>
2025-05-12 23:31:32 +0545

    update script

--- archlinux.sh
+++ archlinux.sh
@@ -4,3 +4,21 @@ sudo pacman -S --needed --noconfirm gdm alacritty nano zsh git timeshift switche
 flatpak update -y
 flatpak install -y org.mozilla.firefox com.mattjakeman.ExtensionManager
 sudo systemctl enable gdm bluetooth switcheroo-control
+
+sudo pacman -S --needed --noconfirm sbctl
+sbctl status
+sudo sbctl create-keys
+sudo sbctl enroll-keys -m
+sbctl status
+sudo sbctl verify
+sudo sbctl sign -s /boot/vmlinuz-linux
+sudo sbctl sign -s /boot/EFI/Linux/arch-linux.efi
+sudo sbctl sign -s /boot/EFI/Linux/arch-linux-fallback.efi
+sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+sudo bootctl install
+sudo sbctl verify
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo pacman -S --needed --noconfirm fastfetch
+clear
+fastfetch
