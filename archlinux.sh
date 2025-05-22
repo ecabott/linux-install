680f7ee73c8db258080e845f7f8cc97e50117a4b
Ayush Bhusal <ayush.bhusal0@gmail.com>
2025-05-22 23:32:55 +0545

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --needed --noconfirm gdm gvfs-google gvfs-mtp ibus-typing-booster alacritty nvim zsh git timeshift switcheroo-control gnome-control-center fwupd nvidia-open gnome-backgrounds flatpak gnome-software noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation gnome-shell-extension-appindicator
+sudo pacman -S --needed --noconfirm gdm gvfs-google gvfs-mtp alacritty nvim zsh git timeshift switcheroo-control gnome-control-center fwupd nvidia-open gnome-backgrounds flatpak gnome-software noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation gnome-shell-extension-appindicator
 flatpak update -y
 flatpak install -y org.mozilla.firefox com.mattjakeman.ExtensionManager
 sudo systemctl enable gdm bluetooth switcheroo-control
